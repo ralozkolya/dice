83d1993d951131dc16f95044e8c13a0bac59947d
Nikoloz Razmadze <raloz1@gmail.com>
2020-07-16 21:22:53 +0400

    Clean-up

--- src/components/Wake/Wake.tsx
+++ src/components/Wake/Wake.tsx
@@ -3,25 +3,37 @@ import NoSleep from 'nosleep.js';
 
 const noSleep = new NoSleep();
 
-export default class Wake extends Component {
+interface IWakeState {
+    value: boolean;
+}
 
-    componentWillUnmount = () => {
-        noSleep.disable();
+export default class Wake extends Component<unknown, IWakeState> {
+
+    state = {
+        value: false
     };
 
-    onChange = ({ currentTarget: { checked } }: SyntheticEvent<HTMLInputElement>) => {
-        if (checked) {
-            noSleep.enable();
-        } else {
-            noSleep.disable();
-        }
+    componentDidMount = (): void => {
+        this.setNoSleep(this.state.value);
+    };
+
+    componentWillUnmount = (): void => {
+        this.setNoSleep(false);
+    };
+
+    onChange = ({ currentTarget: { checked } }: SyntheticEvent<HTMLInputElement>): void => {
+        this.setState({ value: checked }, () => this.setNoSleep(checked));
+    };
+
+    setNoSleep = (enable: boolean): void => {
+        enable ? noSleep.enable() : noSleep.disable();
     };
 
     render() {
         return (
             <div>
                 <label>
-                    <input className="mr-2" type="checkbox" onChange={this.onChange} />
+                    <input className="mr-2" type="checkbox" checked={this.state.value} onChange={this.onChange} />
                     Prevent the screen from turning off
                 </label>
             </div>
