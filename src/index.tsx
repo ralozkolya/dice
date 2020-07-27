a4a362819cff21cb7693097dbf004c8235f92fbe
Nikoloz Razmadze <raloz1@gmail.com>
2020-07-28 01:18:02 +0400

    Return service worker for offline support

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import * as serviceWorker from './serviceWorker';
+
 import App from './components/App/App';
 
 ReactDOM.render(
@@ -10,3 +12,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
